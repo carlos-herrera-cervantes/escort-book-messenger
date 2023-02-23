9594bfea2cdcbd20d0b5137ac0868d6489066709
Carlos Herrera <juan.cervantes@rappi.com>
2023-02-23 10:24:33 -0600

    EB-161 Homologated configuration variables

--- Constants/KafkaTopic.cs
+++
@@ -1,6 +0,0 @@
-namespace EscortBookMessenger.Constants;
-
-public static class KafkaTopic
-{
-    public const string SendEmail = "send-email";
-}
---
+++ Constants/Mail.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EscortBookMessenger.Constants;
+
+public class Mail
+{
+    public static readonly string From = Environment.GetEnvironmentVariable("MAIL_SERVER_FROM");
+
+    public static readonly string Password = Environment.GetEnvironmentVariable("MAIL_SERVER_PASSWORD");
+}
